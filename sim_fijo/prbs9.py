8c82ad5b50354eb56cf8fd35029eb04915d2cb2d
Marcos Raimondi <marcosraimondi1@hotmail.com>
2023-08-12 11:43:56 -0300

    saco bit de prsb9 cuando ya actualize el registro

--- sim_fijo/prbs9.py
+++ sim_fijo/prbs9.py
@@ -14,8 +14,7 @@ def prbs9(seed):
     shift_register = seed & 0b111111111
 
     while True:
-        # la salida es el bit 8 del shift register
-        output_bit = shift_register >> 8
+        # primero shifteo y despues saco la salida con el registro actualizado
 
         # el feedback es el XOR de los bits 8 y 4
         feedback = ((shift_register >> 8) & 1) ^ ((shift_register >> 4) & 1)
@@ -23,4 +22,7 @@ def prbs9(seed):
         # hago el shifteo y agrego el feedback en el bit 0, despues hago and para que quede de 9 bits
         shift_register = ((shift_register << 1) | feedback) & 0b111111111
 
+        # la salida es el bit 8 del shift register
+        output_bit = shift_register >> 8
+        
         yield output_bit
