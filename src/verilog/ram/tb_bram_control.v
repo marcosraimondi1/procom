752b2c5aefe037a2653c681106d67a1ca293f471
marcos raimondi <marcosraimondi1@hotmail.com>
2024-01-30 13:17:00 +0100

    bram_control con testbench, devolviendo un solo pixel

--- src/verilog/ram/tb_bram_control.v
+++ src/verilog/ram/tb_bram_control.v
@@ -12,107 +12,113 @@ module tb_bram_control;
   parameter KERNEL_WIDTH = 3;
 
 
-  // Inputs
-  reg clk;  //! Clock
-  reg reset;  //! Reset
-  reg i_start_loading;
-  reg i_read_for_processing;
+  reg clk;  
+  reg reset;
+
+  reg i_load_valid;
   reg [RAM_WIDTH-1:0] i_data_to_mem;
-  reg [clogb2(RAM_DEPTH-1)-1:0] i_addr_log_to_mem;
-  reg i_valid_get_frame;
-  // Outputs
+  reg i_read_valid;
   wire [RAM_WIDTH-1:0] o_data_from_mem;
-  wire o_is_frame_ready;
-  wire o_valid_data_to_conv;
-  wire [RAM_WIDTH-1:0] o_to_conv[KERNEL_WIDTH-1:0];  //! data to convolver 3 x 8 bits
+
+  reg i_load_valid_2;
+  reg [RAM_WIDTH-1:0] i_data_to_mem_2;
+  reg i_read_valid_2;
+  wire [RAM_WIDTH-1:0] o_data_from_mem_2;
+
+  integer i;
 
   // Clock generation
   always #1 clk = ~clk;
 
-  // Write data to memory
   initial begin
     clk = 0;
-    i_read_for_processing = 0;
-    i_start_loading = 0;
-    i_data_to_mem = 1;
-    i_addr_log_to_mem = 0;
-    i_valid_get_frame = 0;
+    i_read_valid = 0;
+    i_load_valid = 0;
+    i_data_to_mem = 0;
+
+    i_read_valid_2 = 0;
+    i_load_valid_2 = 0;
+    i_data_to_mem_2 = 0;
 
     #1 reset = 1;
     #10 reset = 0;
 
-    // Write data to memory  FUNCIONANDO !!
-    //#2 i_start_loading = 1'b1;
-    //#2 i_start_loading = 1'b0;
+    // Write data to memory
 
-    // Send data to convolver for processing  FUNCIONANDO !!
-    #1 u_bram_control.state_reg = u_bram_control.PROCESS_FRAME;
+    for (i = 0; i < 100; i = i + 1) begin
+      #2 i_load_valid_2 = 1'b1;
+      #2 i_load_valid_2 = 1'b0;
+      #2 i_data_to_mem_2 = i_data_to_mem_2 + 1;
+      
+      #2 i_load_valid = 1'b1;
+      #2 i_load_valid = 1'b0;
+      #2 i_data_to_mem = i_data_to_mem + 1;
+    end
 
-    #2 i_read_for_processing = 1'b1;
+    // Request data for processing
 
-    #600;
+    // discard first read value (repeated 0)
+    #2 i_read_valid = 1'b1;
+    #2 i_read_valid = 1'b0;
+
+    for (i = 0; i < 300-60; i = i + 1) begin
+      #2 i_read_valid = 1'b1;
+      #2 i_read_valid = 1'b0;
+      // $display("Data read from memory: %d", o_data_from_mem);
+    end
+
+     // Get processed frame
+     for (i = 0; i < 100; i = i + 1) begin
+      #2 i_read_valid_2 = 1'b1;
+      #2 i_read_valid_2 = 1'b0;
+      $display("Data read from memory: %d", o_data_from_mem_2);
+    end
+
+    #100;
 
 
     $finish;
-
-    //#2;
-    /*
-    repeat (RAM_DEPTH) begin
-      #2 i_data_to_mem = i_data_to_mem + 1;
-    end
-    #2;
-    if (o_is_frame_ready) begin
-      $display("Memory full");
-      #2 i_addr_log_to_mem = 31;
-
-      #2 i_read_for_processing = 1'b1;
-      #4;
-      $display("Data read from memory: %d", o_data_from_mem);
-      $display("Expected: %d", i_addr_log_to_mem);
-      #2 i_addr_log_to_mem = 32;
-
-      #2 i_read_for_processing = 1'b1;
-      #4;
-      $display("Data read from memory: %d", o_data_from_mem);
-      $display("Expected: %d", i_addr_log_to_mem);
-      #2 i_addr_log_to_mem = 33;
-
-      #2 i_read_for_processing = 1'b1;
-      #4;
-      $display("Data read from memory: %d", o_data_from_mem);
-      $display("Expected: %d", i_addr_log_to_mem);
-      $finish;
-    end else begin
-      $display("Memory not full");
-      $finish;
-      */
-
   end
 
+  while (@posedge clk) begin
+  
+  end
 
-
-  // Instantiate BRAM
+  // Instantiate BRAM for frame to process
   bram_control #(
       .RAM_WIDTH(RAM_WIDTH),
       .RAM_DEPTH(RAM_DEPTH),
       .INIT_FILE(INIT_FILE),
       .KERNEL_WIDTH(KERNEL_WIDTH),
       .IMAGE_WIDTH(IMAGE_WIDTH),
-      .IMAGE_HEIGHT(IMAGE_HEIGHT)
+      .IMAGE_HEIGHT(IMAGE_HEIGHT),
+      .TO_PROCESS(1)
   ) u_bram_control (
-      .clk                  (clk),
-      .reset                (reset),
-      .i_start_loading      (i_start_loading),
-      .i_valid_get_frame    (i_valid_get_frame),
-      .i_read_for_processing(i_read_for_processing),
-      .o_valid_data_to_conv (o_valid_data_to_conv),
-      .i_data_to_mem        (i_data_to_mem),
-      .o_data_from_mem      (o_data_from_mem),
-      .o_is_frame_ready     (o_is_frame_ready),
-      .o_to_conv0           (o_to_conv[0]),
-      .o_to_conv1           (o_to_conv[1]),
-      .o_to_conv2           (o_to_conv[2])
+      .clk              (clk),
+      .reset            (reset),
+      .i_load_valid     (i_load_valid),
+      .i_data_to_mem    (i_data_to_mem),
+      .i_read_valid     (i_read_valid),
+      .o_data_from_mem  (o_data_from_mem)
   );
+
+  // Instantiate BRAM for processed frame
+ bram_control #(
+     .RAM_WIDTH(RAM_WIDTH),
+     .RAM_DEPTH(RAM_DEPTH),
+     .INIT_FILE(INIT_FILE),
+     .KERNEL_WIDTH(KERNEL_WIDTH),
+     .IMAGE_WIDTH(IMAGE_WIDTH),
+     .IMAGE_HEIGHT(IMAGE_HEIGHT),
+     .TO_PROCESS(0)
+ ) u_bram_control_2 (
+     .clk              (clk),
+     .reset            (reset),
+     .i_load_valid     (i_load_valid_2),
+     .i_data_to_mem    (i_data_to_mem_2),
+     .i_read_valid     (i_read_valid_2),
+     .o_data_from_mem  (o_data_from_mem_2)
+ );
 
   //  The following function calculates the address width based on specified RAM depth
   // calcula cuantos bits de direccion hacen falta para direccionar la memoria
