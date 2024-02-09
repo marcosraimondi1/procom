715ba0b6d8a4f18c2946a09011a89a4c651748be
marcos raimondi <marcosraimondi1@hotmail.com>
2024-02-09 13:40:35 +0100

    convolver y tb

--- src/verilog/conv_2d.v
+++ src/verilog/conv_2d.v
@@ -1,5 +1,5 @@
 module conv_2d # (
-    parameter NB_COEFF    = 8,              //Numero de bits de los coeficientes 
+    parameter NB_COEFF    = 8,
     parameter NB_OUTPUT   = 8,
     parameter NB_DATA     = 8,
     parameter KERNEL_SIZE = 9
@@ -7,12 +7,9 @@ module conv_2d # (
  (
     input clk, 
     input i_rst,   
-
     input      signed [NB_COEFF*KERNEL_SIZE-1:0] i_kernel,    
-
     input      signed [NB_DATA*KERNEL_SIZE-1:0] i_data,    
-
-    output     signed [7:0] o_pixel            //Resultado de la convolución
+    output  wire   signed [NB_OUTPUT-1:0] o_pixel            //Resultado de la convolución
     );
 
     localparam NBF_COEFF   = 7;                
@@ -27,19 +24,20 @@ module conv_2d # (
     localparam NBI_OUTPUT = NB_OUTPUT - NBF_OUTPUT;
     localparam NB_SAT     = (NBI_ADD) - (NBI_OUTPUT);
 
-    wire signed [NB_DATA-1:0]  subframe [KERNEL_SIZE:1];   //Sector de la imagen a convolucionar
-    wire signed [NB_COEFF-1:0] kernel   [KERNEL_SIZE:1];    //Matriz de coeficientes del kernel 
-    wire signed [NB_PROD-1:0]  prod     [KERNEL_SIZE:1]; //! Partial Products
+    wire signed [NB_DATA-1:0]  subframe [KERNEL_SIZE-1:0];   //Sector de la imagen a convolucionar
+    wire signed [NB_COEFF-1:0] kernel   [KERNEL_SIZE-1:0];    //Matriz de coeficientes del kernel 
+    wire signed [NB_PROD-1:0]  prod     [KERNEL_SIZE-1:0]; //! Partial Products
 
     genvar gi;
     generate
-        for (gi = 1; gi<KERNEL_SIZE; gi=gi+1) begin
-            assign kernel[gi]   = i_kernel[NB_COEFF*(KERNEL_SIZE-gi-1)-1 -: NB_COEFF];
-            assign subframe[gi] = i_data[NB_DATA*(KERNEL_SIZE-gi-1)-1 -: NB_DATA];
+        for (gi = 0; gi<KERNEL_SIZE; gi=gi+1) begin
+            assign kernel[gi]   = i_kernel[NB_COEFF*(KERNEL_SIZE-gi)-1 -: NB_COEFF];
+            assign subframe[gi] = i_data[NB_DATA*(KERNEL_SIZE-gi)-1 -: NB_DATA];
         end
     endgenerate
 
     // pasar a un for
+    assign prod[0] = subframe[0] * kernel[0];
     assign prod[1] = subframe[1] * kernel[1];
     assign prod[2] = subframe[2] * kernel[2];
     assign prod[3] = subframe[3] * kernel[3];
@@ -48,19 +46,18 @@ module conv_2d # (
     assign prod[6] = subframe[6] * kernel[6];
     assign prod[7] = subframe[7] * kernel[7];
     assign prod[8] = subframe[8] * kernel[8];
-    assign prod[9] = subframe[9] * kernel[9];
 
     reg signed [NB_ADD-1:0] sum;
-
-    always @ (posedge clk) begin
-        if( i_rst ) 
-            sum <= {NB_ADD{1'b0}};
-        else begin 
-            sum <= prod[1]+prod[2]+prod[3]+prod[4]+prod[5]+prod[6]+prod[7]+prod[8]+prod[9];
-        end            
+    always @(posedge clk) begin
+       if (i_rst) begin
+        sum <= {NB_ADD{1'b0}};
+       end
+       else begin
+           sum <= prod[0]+prod[1]+prod[2]+prod[3]+prod[4]+prod[5]+prod[6]+prod[7]+prod[8];
+       end
     end
+    // assign sum = prod[0]+prod[1]+prod[2]+prod[3]+prod[4]+prod[5]+prod[6]+prod[7]+prod[8];
 
     assign o_pixel = ( ~|sum[NB_ADD-1 -: NB_SAT+1] || &sum[NB_ADD-1 -: NB_SAT+1]) ? sum[NB_ADD-(NB_SAT) - 1 -: NB_OUTPUT] :
                       (sum[NB_ADD-1]) ? {{1'b1},{NB_OUTPUT-1{1'b0}}} : {{1'b0},{NB_OUTPUT-1{1'b1}}};
-
 endmodule
--- src/verilog/test_bench/tb_conv_2d.v
+++ src/verilog/test_bench/tb_conv_2d.v
@@ -1,131 +1,102 @@
-//`define INPUT_FILE_PATH "/home/manu/repos/procom/src/python/test1_input.txt"
-//`define RESULT_FILE_PATH "/home/manu/repos/procom/src/python/test1_output.txt"
-`define INPUT_FILE_PATH "C:/Users/agusb/OneDrive/Escritorio/PROCOM/tpfinal/procom/src/verilog/py_input.txt"
-`define RESULT_FILE_PATH "C:/Users/agusb/OneDrive/Escritorio/PROCOM/tpfinal/procom/src/verilog/py_output.txt"
-`define OUTPUT_FILE_PATH "C:/Users/agusb/OneDrive/Escritorio/PROCOM/tpfinal/procom/src/verilog/verilog_output.txt"
 `timescale 1ns/100ps
 
-module tb_conv_2d();
-
-    localparam IMAGE_HEIGHT = 10+2;  //Alto de la imagen + padding
-    localparam IMAGE_WIDTH  = 1+2;  //Ancho de la imagen + padding
+module tb_conv_2d #(
+    parameter NB_COEFF    = 8,
+    parameter NB_OUTPUT   = 8,
+    parameter NB_DATA     = 8,
+    parameter KERNEL_SIZE = 9
+)();
 
     reg clk;
-    reg i_en_conv;
-    reg i_nrst;
-    reg i_load_knl;
-    reg i_data_valid;
+    reg i_rst;
     
-    reg signed [7:0] i_data1;
-    reg signed [7:0] i_data2;
-    reg signed [7:0] i_data3;
-    wire signed [7:0] o_pixel;
+    reg signed [NB_COEFF*3-1:0] kernel_fila2;
+    reg signed [NB_COEFF*3-1:0] kernel_fila1;
+    reg signed [NB_COEFF*3-1:0] kernel_fila0;
 
-    reg [7:0] kernel [9:1];
-    reg [7:0] padded_frame [IMAGE_HEIGHT-1:0][IMAGE_WIDTH-1:0];
-    reg [7:0] conv_image   [(IMAGE_HEIGHT-2)*(IMAGE_WIDTH-2) - 1:0]; //imagen convolucionada (sin padding)
-    reg [31:0] addr;
+    reg signed [NB_DATA*3-1:0] data_fila2;
+    reg signed [NB_DATA*3-1:0] data_fila1;
+    reg signed [NB_DATA*3-1:0] data_fila0;
+    
+    wire     signed [NB_OUTPUT-1:0] o_pixel;
 
-    integer fd_input, fd_result, fd_output;
-    integer i, j;   
+	always #1 clk = ~clk;
 
-	always #5 clk = ~clk;
+    integer i;
 
     initial begin
-       fd_input  = $fopen(`INPUT_FILE_PATH,"r");
-       fd_result  = $fopen(`RESULT_FILE_PATH,"r");
-       fd_output  = $fopen(`OUTPUT_FILE_PATH,"w");
-       if (fd_input && fd_result)
-           $display("Files were opened successfully : %0d %0d %0d",fd_input, fd_result, fd_output);
-       else begin
-           $display("Could not open files successfully : %0d %0d %0d",fd_input, fd_result, fd_output);
-           $finish;
-       end
-        $display("");
-        $display("Simulation Started");
+        clk = 0;
+        i_rst = 0;
 
-        for (j=0;j<IMAGE_WIDTH;j=j+1) begin
-           for (i=0;i<IMAGE_HEIGHT;i=i+1) begin
-               $fscanf(fd_input,"%b\n", padded_frame[i][j]);
-                // $display("%d %d: %b ",i,j, padded_frame[i][j]);
-           end
-       end
+        kernel_fila0 = {8'b0, 8'b0, 8'b0};
+        kernel_fila1 = {8'b0, 8'b1111111, 8'b0};
+        kernel_fila2 = {8'b0, 8'b0, 8'b0};
 
+        data_fila0 = {8'b0, 8'b0, 8'b0};
+        data_fila1 = {8'b0, 8'b0, 8'b0};
+        data_fila2 = {8'b0, 8'b0, 8'b0};
 
-        kernel[1] = 8'b0;
-        kernel[2] = 8'b0;
-        kernel[3] = 8'b0;
-        kernel[4] = 8'b0;
-        kernel[5] = 8'b1111111;
-        kernel[6] = 8'b0;
-        kernel[7] = 8'b0;
-        kernel[8] = 8'b0;
-        kernel[9] = 8'b0;
+        #2 i_rst          = 1;
+        #2 i_rst          = 0;
 
-                clk  = 0;
-                i_nrst = 0;
-                i_en_conv = 0;
-                i_load_knl = 0;
-                i_data_valid = 0;
-                i_data1 = kernel[9];
-                i_data2 = kernel[8];
-                i_data3 = kernel[7];
-        //Cargo el kernel
-        #50     i_nrst = 1;
-                i_load_knl = 1;
-        #10     i_data1 = kernel[6];   
-                i_data2 = kernel[5];  
-                i_data3 = kernel[4];  
-        #10     i_data1 = kernel[3];         
-                i_data2 = kernel[2];  
-                i_data3 = kernel[1];   
-        //Cargo los primeros valores del subframe      
-        #10     i_load_knl = 0;
-                
-		//Cargo los pixeles de a filas
-				for (j=0;j<IMAGE_WIDTH-2;j=j+1) begin
-					for (i=0;i<IMAGE_HEIGHT;i=i+1) begin
-					    i_data1 = padded_frame[i][j];   
-						i_data2 = padded_frame[i][j+1];  
-						i_data3 = padded_frame[i][j+2];  
-                        if(i > 2)
-                            i_data_valid = 1;   //se cargaron las 3 filas
-                        else i_data_valid = 0;
-                        // if(i_data_valid) begin
-                        //     $fdisplay(fd_output, "%b\n", o_pixel);
-                        //     $display("%b\n", o_pixel);
-                        // end
-                        #10;
-                	end
-                end
-				i_data1 = 8'b0; 
-				i_data2 = 8'b0;
-				i_data3 = 8'b0;
+        #3;
+        
+        #2 data_fila1 = {8'b0, 8'h1, 8'b0};
+        #2;
+        $display("expected = %d , out = %d", data_fila1[15:8], o_pixel); 
 
-        #60     $fclose(fd_input);
-                $fclose(fd_result);
-                $fclose(fd_output);
-                $finish;
+        #2 data_fila1 = {8'b1, 8'h2, 8'b0};
+        #2;
+        $display("expected = %d , out = %d", data_fila1[15:8], o_pixel); 
+
+        #2 data_fila1 = {8'b0, 8'h3, 8'b0};
+        #2;
+        $display("expected = %d , out = %d", data_fila1[15:8], o_pixel); 
+
+        #2 data_fila1 = {8'b0, 8'h4, 8'b1};
+        #2;
+        $display("expected = %d , out = %d", data_fila1[15:8], o_pixel); 
+
+        #2 data_fila1 = {8'b0, 8'h5, 8'b0};
+        data_fila2 = {8'b0, 8'h6, 8'b0};
+        #2;
+        $display("expected = %d , out = %d", data_fila1[15:8], o_pixel); 
+
+        #2 data_fila1 = {8'b0, 8'h6, 8'b0};
+        data_fila2 = {8'b1, 8'h6, 8'b1};
+        #2;
+        $display("expected = %d , out = %d", data_fila1[15:8], o_pixel); 
+
+        #2 data_fila1 = {8'b0, 8'h7, 8'b0};
+        #2;
+        $display("expected = %d , out = %d", data_fila1[15:8], o_pixel); 
+
+        #20;
+        $finish;
     end
+
     
-    always @ (posedge clk) begin
-        if(i_data_valid) begin
-            $fdisplay(fd_output, "%b", o_pixel);
-            $display("%b", o_pixel);
-        end
-    end
 
-    conv_2d u_conv(
-        .clk(clk),
-        .i_en_conv(i_en_conv),
-        .i_nrst(i_nrst),
-        .i_load_knl(i_load_knl),
-        .i_data_valid(i_data_valid),
-        .i_data1(i_data1),
-        .i_data2(i_data2),
-        .i_data3(i_data3),
-        .o_pixel(o_pixel)
-    );
+    wire     signed [NB_COEFF*KERNEL_SIZE-1:0] i_kernel;    
+    wire     signed [NB_DATA*KERNEL_SIZE-1:0] i_data;    
+
+    
+    assign i_kernel = {kernel_fila2, kernel_fila1, kernel_fila0};
+    assign i_data = {data_fila2, data_fila1, data_fila0};
+
+    conv_2d # (
+        .NB_COEFF    (NB_COEFF),
+        .NB_OUTPUT   (NB_OUTPUT),
+        .NB_DATA     (NB_DATA),
+        .KERNEL_SIZE (KERNEL_SIZE)
+    )
+    u_conv_2d (
+        .clk(clk), 
+        .i_rst(i_rst),   
+        .i_kernel(i_kernel),    
+        .i_data(i_data),    
+        .o_pixel(o_pixel)            
+        );
 
 
 endmodule
