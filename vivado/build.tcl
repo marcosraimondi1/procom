3039650d9bbe79be9c305dc8f1b0bf5900b8cbe0
marcos raimondi <marcosraimondi1@hotmail.com>
2024-02-10 19:55:02 +0100

    tcl ip_repo dir updated

--- vivado/build.tcl
+++ vivado/build.tcl
@@ -135,7 +135,7 @@ if {[string equal [get_filesets -quiet sources_1] ""]} {
 # Set IP repository paths
 set obj [get_filesets sources_1]
 if { $obj != {} } {
-   set_property "ip_repo_paths" "[file normalize "$origin_dir/ip_repo/axi_stream/axi_stream.srcs/sources_1/new"]" $obj
+   set_property "ip_repo_paths" "[file normalize "$origin_dir/ip_repo"]" $obj
 
    # Rebuild user ip_repo's index before adding any source files
    update_ip_catalog -rebuild
