8fe625a0bd03956f20c93716e96c41eb4b2ea386
marcos raimondi <marcosraimondi1@hotmail.com>
2024-02-10 19:52:56 +0100

    C para el convolver

--- src/C/udp_echoserver.c
+++ src/C/udp_echoserver.c
@@ -28,15 +28,17 @@ void process_data(struct pbuf* p)
 	register int a0;
 	register int a1;
 
-	int i;
+	int i, j, aux_i;
 	int metadata_bytes_left = METADATA_SIZE;
 
+	struct pbuf *prev_p = 0;
 	u8 *payload = p->payload;
+	u8 *aux_payload = 0;
 
-	// los bytes los lee como ascii byte=0x01 => 49 byte=0x00 => 48
 	u8 kernel = payload[0] | payload[1]<<1;
 
-	// xil_printf("kernel: %d\n", kernel);
+	u8 byte_count = 0;
+	u32 value = 0;
 
 	while (p!=NULL)
 	{
@@ -49,12 +51,41 @@ void process_data(struct pbuf* p)
 			}
 			else
 			{
-				a0 = payload[i];
-				putfslx(a0,  0, FSL_DEFAULT);
-				getfslx(a1,  0, FSL_DEFAULT);
-				payload[i] = a1;
+				// cada elemento de payload es un byte (1pixel)
+				// cuento 4 y los mando en un solo valor de 32 bits
+				// hay que tener en cuenta que el puntero de payload puede cambiar
+				// por eso almaceno el puntero al payload anterior
+
+				value = value | (payload[i]<<(8*byte_count));
+				payload[i] = 0; // elimino para ver si realmente funciona
+				byte_count += 1;
+
+				if (byte_count == 4){
+					a0 = value;
+					putfslx(a0,  0, FSL_DEFAULT);
+					getfslx(a1,  0, FSL_DEFAULT);
+
+					aux_payload = payload;
+					aux_i = i;
+					for (j=0; j<4; j+=1)
+					{
+						if ((aux_i-j) < 0)
+						{
+							// el indice corresponde al payload anterior
+							aux_payload = prev_p->payload;
+							aux_i = (prev_p->len - 1) + j;
+						}
+
+						// extraer pixeles y guardarlos en el payload
+						aux_payload[aux_i-j] = ( a1 >> (8*(3-j)) ) & 0xFF;
+					}
+
+					byte_count = 0;
+					value = 0;
+				}
 			}
 		}
+		prev_p = p;
 		p = p->next;
 	}
 }
